1f0b508b228bc2a8fbf3603331b2b6e7e45e605d
Christian Leyva <chrisley304@hotmail.com>
2022-12-29 19:30:58 -0600

    Cambio de color a oscuro con idea de darkMode

--- lib/constans/colors.dart
+++ lib/constans/colors.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
-const mainColor = Color.fromRGBO(225, 229, 242, 1);
+// const mainColor = Color.fromRGBO(225, 229, 242, 1);
+const mainColor = Color.fromRGBO(18, 18, 18, 1);
 
 const secondaryColor = Color.fromRGBO(19, 78, 111, 1);
